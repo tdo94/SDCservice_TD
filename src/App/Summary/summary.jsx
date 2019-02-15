56624069acccaafbbdd61f27bf156e29e21c9202
Andrew Mitchell <andrewgeorgemitchell@gmail.com>
2019-02-15 14:51:03 -0800

    Fix PropType validation in summary comp

--- src/App/Summary/summary.jsx
+++ src/App/Summary/summary.jsx
@@ -13,16 +13,13 @@ import {
 const Summary = ({ product }) => {
   const priceStrikeThrough = <span className="strikeThrough">{`$${product.price - 0.01}`}</span>;
   const newDescriptionArr = [];
-  let newDescription;
-  if (product.description) {
-    newDescription = product.description
-      .replace('\n', '')
-      .split('.');
-    for (let i = 0; i < newDescription.length; i += 1) {
-      newDescription[i] = newDescription[i].replace(/^\s+|\s+$/g, '');
-      if (newDescription[i] !== '') {
-        newDescriptionArr.push(<li key={i}><span>{newDescription[i]}</span></li>);
-      }
+  const newDescription = product.description
+    .replace('\n', '')
+    .split('.');
+  for (let i = 0; i < newDescription.length; i += 1) {
+    newDescription[i] = newDescription[i].replace(/^\s+|\s+$/g, '');
+    if (newDescription[i] !== '') {
+      newDescriptionArr.push(<li key={i}><span>{newDescription[i]}</span></li>);
     }
   }
   return (
@@ -92,48 +89,25 @@ const Summary = ({ product }) => {
 
 Summary.propTypes = {
   product: PropTypes.shape({
-    id: PropTypes.number,
-    unique_id: PropTypes.number,
-    name: PropTypes.string,
-    category: PropTypes.string,
-    manufacturer: PropTypes.string,
-    primary_image: PropTypes.string,
-    review_one_star_count: PropTypes.number,
-    review_two_star_count: PropTypes.number,
-    review_three_star_count: PropTypes.number,
-    review_four_star_count: PropTypes.number,
-    review_five_star_count: PropTypes.number,
-    review_count: PropTypes.number,
-    question_count: PropTypes.number,
-    price: PropTypes.number,
-    total_price: PropTypes.number,
-    stock: PropTypes.number,
-    is_prime: PropTypes.bool,
-    description: PropTypes.string,
-  }),
-};
-
-Summary.defaultProps = {
-  product: {
-    id: 1,
-    unique_id: 1,
-    name: 'Amazon Product 1',
-    category: 'electronics',
-    manufacturer: 'Murazik and Sons',
-    primary_image: 'https://s3.us-east-2.amazonaws.com/product-summary-component/electronics1.jpg',
-    review_one_star_count: 624,
-    review_two_star_count: 639,
-    review_three_star_count: 622,
-    review_four_star_count: 275,
-    review_five_star_count: 251,
-    review_count: 2411,
-    question_count: 216,
-    price: 561,
-    total_price: 571,
-    stock: 5,
-    is_prime: true,
-    description: 'string',
-  },
+    id: PropTypes.number.isRequired,
+    unique_id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    manufacturer: PropTypes.string.isRequired,
+    primary_image: PropTypes.string.isRequired,
+    review_one_star_count: PropTypes.number.isRequired,
+    review_two_star_count: PropTypes.number.isRequired,
+    review_three_star_count: PropTypes.number.isRequired,
+    review_four_star_count: PropTypes.number.isRequired,
+    review_five_star_count: PropTypes.number.isRequired,
+    review_count: PropTypes.number.isRequired,
+    question_count: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    total_price: PropTypes.number.isRequired,
+    stock: PropTypes.number.isRequired,
+    is_prime: PropTypes.bool.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Summary;
