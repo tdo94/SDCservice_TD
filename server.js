9aa3256405fd64e8e7d2ee43feabc4bf93ad44aa
Andrew Mitchell <andrewgeorgemitchell@gmail.com>
2019-02-19 14:14:13 -0800

    v2.2 Hotfix

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const productRoutes = require('./app/controllers/productRoutes');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+app.use(express.static(`${__dirname}/dist`));
 app.use('/api/products', cors(), productRoutes);
+
+app.get('*', (request, response) => {
+  response.sendFile(`${__dirname}/dist/index.html`);
+});
+
 
 app.listen(3001);
 console.log('Listening on localhost:3001');
